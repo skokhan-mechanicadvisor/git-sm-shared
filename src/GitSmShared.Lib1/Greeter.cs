ec02b5b728a2d914468f96719e90390284d43c04
Serhii Kokhan <serhii@mechanicadvisor.com>
2020-09-07 13:22:05 +0300

    Initial commit

---
+++ src/GitSmShared.Lib1/Greeter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitSmShared.Lib1
+{
+    public class Greeter
+    {
+        public void Do()
+        {
+            Console.WriteLine($"{DateTime.Now}::Hello From GitSmShared.Lib1");
+        }
+    }
+}
